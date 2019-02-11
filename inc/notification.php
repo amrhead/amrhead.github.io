4345bde3d3e629fd21b6753ddc9a0480d74d14e2
amrhead <appxds@gmail.com>
2019-02-11 18:59:06 +0300

    Delete notification.php

--- inc/notification.php
+++
@@ -1,6 +0,0 @@
-<?php 
-if(isset($_SESSION['flash_message'])) {
-	echo '<div class="notification" title="Click to close">' . $_SESSION['flash_message'] .'</div>';
-	unset($_SESSION['flash_message']);
-}
-?>
